159fc321b0695fd31fb6993120af9927c205dfbf
lordchilljack <lordchilljack@gmail.com>
2018-04-12 16:36:15 +0800

    Simplify

--- FatFapFapen/Assets/Script/TurnVertical.cs
+++ FatFapFapen/Assets/Script/TurnVertical.cs
@@ -3,61 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurnVertical : MonoBehaviour {
-	public float RotationSpeed = 10;
-	public float DampingTime =0.2f;
-	private Vector3 m_TargetAngles;
-	private Vector3 m_FollowAngles;
-	private Vector3 m_FollowVelocity;
-	private Quaternion m_OriginalRotation;
-	private static Vector2 rotationRange = new Vector2(100, -200);
-	private static Vector2 _rotationRangeLower = new Vector2();
-	public static Vector2 rotationRangeLower
+	public float rotationRange = 100;
+	public float rotationSpeed = 2.5f;
+	public float dampingTime = 0.2f;
+
+	private Vector3 targetAngles;
+	private Vector3 followAngles;
+	private Vector3 followVelocity;
+	private Quaternion originalRotation;
+
+	void Start()
 	{
-		set
-		{
-			if (value != new Vector2()) {
-				_rotationRangeLower = value;
-			} else {
-				_rotationRangeLower = -rotationRange;
-			}
-		}
-	}
-	// Use this for initialization
-	void Start () {
-		_rotationRangeLower = -rotationRange;
-		m_OriginalRotation = transform.localRotation;
+		originalRotation = transform.localRotation;
 	}
 
-	// Update is called once per frame
-	void Update () {
-		float inputV;
-		if (m_TargetAngles.y > 90){
-			m_TargetAngles.y -= 180;
-			m_FollowAngles.y -= 180;
-		}
-		if (m_TargetAngles.x > 90){
-			m_TargetAngles.x -= 180;
-			m_FollowAngles.x -= 360;
-		}  
-		if (m_TargetAngles.y < -90){
-			m_TargetAngles.y += 180;
-			m_FollowAngles.y += 180;
-		}
-		if (m_TargetAngles.x < -90){
-			m_TargetAngles.x += 180;
-			m_FollowAngles.x += 180;
-		}
+	void Update()
+	{
+		transform.localRotation = originalRotation;
 
-		inputV = Input.GetAxis("Vertical");
-		m_TargetAngles.x += inputV*RotationSpeed;
+		float inputVertical = Input.GetAxis("Vertical");
 
-		// clamp values to allowed range
-		m_TargetAngles.x = Mathf.Clamp(m_TargetAngles.x, _rotationRangeLower.x*0.5f, rotationRange.x*0.5f);
+		targetAngles.x += inputVertical * rotationSpeed;
+		targetAngles.x = Mathf.Clamp(targetAngles.x, -rotationRange * 0.5f, rotationRange * 0.5f);
 
-		// smoothly interpolate current values to target angles
-		m_FollowAngles = Vector3.SmoothDamp(m_FollowAngles, m_TargetAngles, ref m_FollowVelocity, DampingTime);
-
-		// update the actual gameobject's rotation
-		transform.localRotation = m_OriginalRotation*Quaternion.Euler(-m_FollowAngles.x, m_FollowAngles.y, 0);
+		followAngles = Vector3.SmoothDamp(followAngles, targetAngles, ref followVelocity, dampingTime);
+		transform.localRotation = originalRotation * Quaternion.Euler(-followAngles.x, followAngles.y, 0);
 	}
 }
