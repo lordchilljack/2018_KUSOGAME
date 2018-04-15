e34996b90f99fef5f43cc3950aa8ddccf2f5dae3
shiftsmart <shiftsmart@gmail.com>
2018-04-15 15:52:19 +0800

    endinf_fin

---
+++ FatFapFapen/Assets/Script/ending_ctrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ending_ctrl : MonoBehaviour {
+    public TitleCtrl tc;
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+
+    public void menuGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ReGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
